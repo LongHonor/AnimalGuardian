0b7990b05b763e2579dae0f547cbcf07442f9cdf
rlakim <39685211+rlakim@users.noreply.github.com>
2022-11-20 15:50:23 +0900

    Add files via upload
    add cursor handler

---
+++ AnimalGuardian/globalVariable.c
@@ -0,0 +1,70 @@
+#include "globalVariable.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+//PC ÁÂÇ¥ ÇÚµé·¯
+PC getPCCurrentPos() {
+	return real_pc;
+}
+void setPCCurrentPos( int moveX, int moveY) {
+	real_pc.pos.X = moveX; real_pc.pos.Y = moveY;
+}
+
+//Enemy NPC ÁÂÇ¥ ÇÚµé·¯
+Pos getEnemyCurrentPos(EnemyNPC * enemyNPC) {
+	Pos curPos;
+	curPos.X = enemyNPC->pos.X; curPos.Y = enemyNPC->pos.Y;
+	return curPos;
+}
+void setEnemyCurrentPos(EnemyNPC * enemyNPC, int moveX, int moveY) {
+	enemyNPC->pos.X = moveX; enemyNPC->pos.Y = moveY;
+}
+
+
+//Animal NPC ÁÂÇ¥ ÇÚµé·¯
+Pos getAnimalCurrentPos(AnimalNPC * animalNPC) {
+	Pos curPos;
+	curPos.X = animalNPC->pos.X; curPos.Y = animalNPC->pos.Y;
+	return curPos;
+}
+void setAnimalCurrentPos(AnimalNPC * animalNPC, int moveX, int moveY) {
+	animalNPC->pos.X = moveX; animalNPC->pos.Y = moveY;
+}
+
+//bullet ¸ðµ¨
+char bulletModel[2][1] =
+	{	{1},
+		{1}	 };
+
+//Ä¿¼­
+int curPosX = GBOARD_O_X, curPosY = GBOARD_O_Y;
+
+void removeCursor()
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curInfo.bVisible = 0;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+}
+
+void setCurrentCursorPos(int posX, int posY)
+{
+	COORD pos = { posX, posY };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	curPosX = posX;
+	curPosY = posY;
+}
+
+COORD getCurrentCursorPos(void) {
+	COORD curPoint;
+	CONSOLE_SCREEN_BUFFER_INFO curInfo;
+
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curPoint.X = curInfo.dwCursorPosition.X;
+	curPoint.Y = curInfo.dwCursorPosition.Y;
+
+	return curPoint;
+}
+
---
+++ AnimalGuardian/globalVariable.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <windows.h>
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define SPACE 32
+#define ATTACK 82
+#define ITEM 81
+
+#define DoubleSpeed 115
+#define GBOARD_WIDTH 40
+#define GBOARD_HEIGHT 20
+#define GBOARD_O_X 4
+#define GBOARD_O_Y 2
+
+//gameboard 2차원 배열
+int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
+
+//Position
+typedef struct _Position {
+	int X;
+	int Y;
+} Pos;
+
+//PC
+typedef struct _PC {
+	int speed;
+	int gun_load_speed;
+	Pos pos;
+	int life_point;
+} PC;
+
+//NPC - enemy
+typedef struct _EnemyNPC {
+	int speed;
+	Pos pos;
+	int id;
+} EnemyNPC;
+
+
+//NPC - animal
+typedef struct _AnimalNPC {
+	int speed;
+	Pos pos;
+	int id;
+}AnimalNPC;
+
+//bullet
+char bulletModel[2][1];
+
+//UI
+int enemy_number;
+int bullet_number;
+int item_number;
+int score;
+
+//one-player 전역변수
+PC real_pc;
+
+//test용 NPC 전역변수
+AnimalNPC temp_animal;
+EnemyNPC temp_enemy;
+
+//커서
+int curPosX ,curPosY;
+
+//함수 선언부
+PC getPCCurrentPos();
+void setPCCurrentPos(int moveX, int moveY);
+Pos getEnemyCurrentPos(EnemyNPC * enemyNPC);
+void setEnemyCurrentPos(EnemyNPC * enemyNPC, int moveX, int moveY);
+Pos getAnimalCurrentPos(AnimalNPC * animalNPC);
+void setAnimalCurrentPos(AnimalNPC * animalNPC, int moveX, int moveY);
+
+void removeCursor();
+void setCurrentCursorPos(int posX, int posY);
+COORD getCurrentCursorPos(void);
