9e7afbbec64309435706dbf9e09b8856dd369607
arkjackson <111726392+arkjackson@users.noreply.github.com>
2022-11-20 16:05:41 +0900

    Update header.h

--- header.h
+++ header.h
@@ -6,15 +6,19 @@
 #define SPACE 32
 #define ATTACK 82
 #define ITEM 81
+#define LEFT 75
+#define RIGHT 77
 
 #define DoubleSpeed 115
+#define GBOARD_O_X 0
+#define GBOARD_O_Y 0
+#define gameBoardWidth 40
+#define gameBoardHeight 20
 #define GBOARD_WIDTH 40
 #define GBOARD_HEIGHT 20
-#define GBOARD_O_X 4
-#define GBOARD_O_Y 2
 
 //gameboard 2차원 배열
-int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
+extern int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
 
 //Position
 typedef struct _Position {
@@ -29,6 +33,7 @@ typedef struct _PC {
 	Pos pos;
 	int life_point;
 } PC;
+extern char pcModel[][1];
 
 //NPC - enemy
 typedef struct _EnemyNPC {
@@ -46,27 +51,36 @@ typedef struct _AnimalNPC {
 }AnimalNPC;
 
 //bullet
-char bulletModel[1][2];
+extern char bulletModel[][1];
 
 //UI
-int enemy_number;
-int bullet_number;
-int item_number;
-int score;
+extern int enemy_number;
+extern int bullet_number;
+extern int item_number;
+extern int score;
 
 //one-player 전역변수
-PC real_pc;
+extern PC pc;
 
 //커서
-int curPosX ,curPosY;
+extern int curPosX, curPosY;
 
 //함수 선언부
-PC getPCCurrentPos();
-void setPCCurrentPos(int moveX, int moveY);
-Pos getEnemyCurrentPos(EnemyNPC * enemyNPC);
-void setEnemyCurrentPos(EnemyNPC * enemyNPC, int moveX, int moveY);
-Pos getAnimalCurrentPos(AnimalNPC * animalNPC);
-void setAnimalCurrentPos(AnimalNPC * animalNPC, int moveX, int moveY);
+//PC getPCCurrentPos();
+//void setPCCurrentPos(int moveX, int moveY);
+Pos getEnemyCurrentPos(EnemyNPC* enemyNPC);
+void setEnemyCurrentPos(EnemyNPC* enemyNPC, int moveX, int moveY);
+Pos getAnimalCurrentPos(AnimalNPC* animalNPC);
+void setAnimalCurrentPos(AnimalNPC* animalNPC, int moveX, int moveY);
 
 void removeCursor(void);
 void setCurrentCursorPos(int posX, int posY);
+void initGameBoard();
+//void drawPC();
+void drawGameBoard();
+void showPc(char modelinfo[][1], PC pc);
+void deletePc(char modelinfo[][1], PC pc);
+int detectCollisionPC(int posX, int posY, char blockModel[][1]);
+int shiftLeftPc();
+int shiftRightPc();
+void processKeyInput();
