7074d0c55e871a7101ef90d16bc805113e26ba90
rlakim <39685211+rlakim@users.noreply.github.com>
2022-11-22 22:01:53 +0900

    camel case 형식으로 수정, getPCCurrentPos 리턴값 변경
    enemyNPC 충돌관리 함수 구현 추가

--- AnimalGuardian/globalVariable.c
+++ AnimalGuardian/globalVariable.c
@@ -1,46 +1,61 @@
-﻿#include "globalVariable.h"
+#include "globalVariable.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 
+//모델
+char bulletModel[2][1] ={ 
+	{1},
+	{1} 
+};
+
+char pcModel[2][1] = {
+	{1},
+	{2}
+};
+
+char animalModel[1][2] = {
+	{1, 2}
+};
+
+char enemyModel[2][1] = {
+	{1},
+	{2}
+};
+
+//커서 전역변수
+int curPosX = GBOARD_O_X, curPosY = GBOARD_O_Y;
 
 //PC 좌표 핸들러
-PC getPCCurrentPos() {
-	return real_pc;
+pos getPCCurrentPos() {
+	return tempPc.pos;
 }
 void setPCCurrentPos( int moveX, int moveY) {
-	real_pc.pos.X = moveX; real_pc.pos.Y = moveY;
+	tempPc.pos.X = moveX; tempPc.pos.Y = moveY;
 }
 
 //Enemy NPC 좌표 핸들러
-Pos getEnemyCurrentPos(EnemyNPC * enemyNPC) {
-	Pos curPos;
+pos getEnemyCurrentPos(enemyNPC * enemyNPC) {
+	pos curPos;
 	curPos.X = enemyNPC->pos.X; curPos.Y = enemyNPC->pos.Y;
 	return curPos;
 }
-void setEnemyCurrentPos(EnemyNPC * enemyNPC, int moveX, int moveY) {
+void setEnemyCurrentPos(enemyNPC * enemyNPC, int moveX, int moveY) {
 	enemyNPC->pos.X = moveX; enemyNPC->pos.Y = moveY;
 }
 
 
 //Animal NPC 좌표 핸들러
-Pos getAnimalCurrentPos(AnimalNPC * animalNPC) {
-	Pos curPos;
+pos getAnimalCurrentPos(animalNPC * animalNPC) {
+	pos curPos;
 	curPos.X = animalNPC->pos.X; curPos.Y = animalNPC->pos.Y;
 	return curPos;
 }
-void setAnimalCurrentPos(AnimalNPC * animalNPC, int moveX, int moveY) {
+void setAnimalCurrentPos(animalNPC * animalNPC, int moveX, int moveY) {
 	animalNPC->pos.X = moveX; animalNPC->pos.Y = moveY;
 }
 
-//bullet 모델
-char bulletModel[2][1] =
-	{	{1},
-		{1}	 };
-
-//커서
-int curPosX = GBOARD_O_X, curPosY = GBOARD_O_Y;
-
+//커서 삭제
 void removeCursor()
 {
 	CONSOLE_CURSOR_INFO curInfo;
@@ -49,6 +64,7 @@ void removeCursor()
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 
+//커서 위치 설정
 void setCurrentCursorPos(int posX, int posY)
 {
 	COORD pos = { posX, posY };
@@ -57,6 +73,7 @@ void setCurrentCursorPos(int posX, int posY)
 	curPosY = posY;
 }
 
+//현재 커서 위치 반환
 COORD getCurrentCursorPos(void) {
 	COORD curPoint;
 	CONSOLE_SCREEN_BUFFER_INFO curInfo;
@@ -68,3 +85,19 @@ COORD getCurrentCursorPos(void) {
 	return curPoint;
 }
 
+//enemyNPC 충돌 관리
+int enemyNPCDetectCollision(int posX, int posY) {
+	int x, y;
+	int arrX = (posX - GBOARD_O_X) / 2;
+	int arrY = posY - GBOARD_O_Y;
+	for (x = 0; x < 1; x++)
+	{
+		for (y = 0; y < 2; y++)
+		{
+			if (enemyModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 1) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
