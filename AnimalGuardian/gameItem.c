17b1dbb551a49a17b792ee4e488df02baf6a4619
LongHonor <kyh011039@gmail.com>
2022-11-25 00:53:49 +0900

    enemy speed 조절 아이템 논리 구현

--- AnimalGuardian/gameItem.c
+++ AnimalGuardian/gameItem.c
@@ -3,20 +3,38 @@
 
 void reduceEnemySpeed() {
 	//enemy배열 받아와서 속도 값 세팅
+	enemyNPC* p = NULL;
+	int reducedSpeed = 0;
+
+	if (enemyList == NULL) return;
+	while (p != NULL) {
+		p->speed = reducedSpeed;
+		p = p->next;
+	}
 }
 
 void restoreEnemySpeed() {
 	//enemy배열 받아와서 속도 값 초기상태로 세팅
+	enemyNPC* p = NULL;
+	int restoredSpeed = 0;
+
+	if (enemyList == NULL) return;
+	while (p != NULL) {
+		p->speed = restoredSpeed;
+		p = p->next;
+	}
 }
 
 void modifyReloadSpeed(int status) {
 	//status에 따라 재장전 속도 증감
+	//증가 : 1, 감소 : 0;
 	if (status == 1);
 	else;
 }
 
 void restoreReloadSpeed() {
 	//재장전 속도 초기상태로 세팅
+	
 }
 
 int isBarricadePlaceable() {
