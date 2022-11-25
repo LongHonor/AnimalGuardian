c2c67d1d4adfce45fc4f9972dbf889ce87e85c18
DESKTOP-NNRHRSC\82104 <jackson0511@naver.com>
2022-11-25 15:57:25 +0900

    pc 좌우이동, 총 발사 구현

--- AnimalGuardian/main.c
+++ AnimalGuardian/main.c
@@ -3,27 +3,32 @@
 #include "globalVariable.h"
 #include "gameBoardHandler.h"
 #include "gameItem.h"
-
+#include "pcHandler.h"
 
 int main() {
 	
 	removeCursor();
 	drawGameBoard();
-	makeEnemyList();
-	makeEnemy();
-	setPCCurrentPos(10, 20);
+	//makeEnemyList();
+	//makeEnemy();
+	//setPCCurrentPos(10, 20);
 	setAnimalCurrentPos(&animalArray, 20, 1);
-	
-	while (1) {
-		drawPC();
+	drawAnimal();
+	/*while (1) {
+		drawPC(player);
 		drawAnimal();
 		drawEnemy();
 		Sleep(200);
-		deletePC();
+		deletePC(player);
 		deleteAnimal();
 		deleteEnemy();
 		Sleep(200);
-	}
+	}*/
 
+	//키보드 입력 및 pc 구현
+	drawPC(player);
+	while (1) {
+		pcKeyInput();
+	}
 	return 0;
 }
