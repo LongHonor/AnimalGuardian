1e01b46bb7d22cf66209ecc4d20e93b5ec5edb48
DESKTOP-NNRHRSC\82104 <jackson0511@naver.com>
2022-11-25 15:53:49 +0900

    drawPC, deletePC -> pcHandler에서 처리

--- AnimalGuardian/gameBoardHandler.h
+++ AnimalGuardian/gameBoardHandler.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
 void initGameBoard();
-void drawGameBoard();
-void drawPC();
-void deletePC();
+void drawGameBoard();	
+//void drawPC();	//pc 핸들러에서 처리
+//void deletePC();
 void drawAnimal();
 void deleteAnimal();
 void drawEnemy();
