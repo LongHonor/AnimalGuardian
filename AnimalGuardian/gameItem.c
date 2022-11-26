65f962d9ff505520d99fb9d7028f7dd34391aac8
rlakim <39685211+rlakim@users.noreply.github.com>
2022-11-27 00:46:58 +0900

    barricade module 실행 부분
    상하좌우&스페이스 키

--- AnimalGuardian/gameItem.c
+++ AnimalGuardian/gameItem.c
@@ -1,5 +1,11 @@
 ﻿#include "gameItem.h"
 #include "globalVariable.h"
+#include "barricadeHandler.h"
+#include "gameBoardHandler.h"
+#include <conio.h>
+
+//temporal code
+#define down 80
 
 void reduceEnemySpeed() {
 	//enemy배열 받아와서 속도 값 세팅
@@ -39,18 +45,17 @@ void restoreReloadSpeed() {
 	setPcReloadSpeed(initialReloadSpeed);
 }
 
-boolean isBarricadePlaceable(int barcX, int barcY) {
+boolean isBarricadePlaceable(int posX, int posY) {
 	//인자 바리케이드 좌표
 	//게임보드 전역변수 게터로 가져와서 설치 가능한지 확인
 	// 0 : false 1 : true
 
 	setCurrentGameBoard(0);
-
-	char barcModel[2] = { {1},{1} };
 	int i;
-
-	for (i = 0; i < 2; i++) {
-		if (currentGameBoard[barcY][barcX + i] != 0) return FALSE;
+	int arrX = (posX - gBoardOx) / 2;
+	int arrY = posY - gBoardOy;
+	for (i = 0; i < 3; i++) {
+		if (currentGameBoard[arrY][arrX + i] != 0) return FALSE;
 	}
 
 	return TRUE;
@@ -66,4 +71,38 @@ void placeBarricade() {
 	//설치 가능하면 게임보드 갱신
 	//불가능하면 경고 메시지 출력
 	//이동키 누르면 바리케이드 위치 이동
+
+	int key;
+	drawBarricade();
+	while (1) {
+		for (int i = 0; i < 20; i++) {
+			//_kbhit 은 입력시 1 리턴
+			if (_kbhit() != 0) {
+				key = _getch();
+				switch (key) {
+				case left:
+					shiftLeftBarricade();
+					break;
+				case right:
+					shiftRightBarricade();
+					break;
+				case up:
+					shiftUpBarricade();
+					break;
+				case down:
+					shiftDownBarricade();
+					break;
+				case space:
+					
+					if (isBarricadePlaceable(barcX, barcY) == 1) {
+						posStruct barricadeCurPos = { barcX,barcY };
+						AddBlockToBoard(barricadeCurPos);
+						return;
+					}
+					break;
+				}
+			}
+			Sleep(20);
+		}
+	}
 }
