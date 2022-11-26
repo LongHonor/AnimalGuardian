c3d6107c6b6d92256a0499df8937fdcf8e6893cb
rlakim <39685211+rlakim@users.noreply.github.com>
2022-11-27 00:32:05 +0900

    remove ^M

--- AnimalGuardian/detectCollision.c
+++ AnimalGuardian/detectCollision.c
@@ -3,8 +3,11 @@
 #include "barricadeHandler.h"
 
 //enemyNPC 충돌 관리
-int enemyNPCDetectCollision(int posX, int posY) {	int x, y;	int arrX = (posX - gBoardOx) / 2;
-	int arrY = posY - gBoardOy;	for (x = 0; x < 1; x++)
+int enemyNPCDetectCollision(int posX, int posY) {
+	int x, y;
+	int arrX = (posX - gBoardOx) / 2;
+	int arrY = posY - gBoardOy;
+	for (x = 0; x < 1; x++)
 	{
 		for (y = 0; y < 2; y++)
 		{
@@ -17,7 +20,12 @@ int enemyNPCDetectCollision(int posX, int posY) {	int x, y;	int arrX = (posX -
 				return 9;
 			}
 		}
-	}	return 1;}int detectCollisionPC(int posX, int posY) {
+	}
+	return 1;
+}
+
+
+int detectCollisionPC(int posX, int posY) {
 	int arrx = (posX - gBoardOx) / 2;
 	int arry = (posY - gBoardOy);
 	//pc총돌 리턴 0
@@ -37,8 +45,13 @@ int detectCollisionBullet(int posX, int posY) {
 		return 0;
 	}
 	return 1;
-}int detectCollisionBarricade(int posX, int posY) {	int x, y;	int arrX = (posX - gBoardOx) / 2;
-	int arrY = posY - gBoardOy;	for (x = 0; x < 3; x++)
+}
+
+int detectCollisionBarricade(int posX, int posY) {
+	int x, y;
+	int arrX = (posX - gBoardOx) / 2;
+	int arrY = posY - gBoardOy;
+	for (x = 0; x < 3; x++)
 	{
 		for (y = 0; y < 1; y++)
 		{
@@ -46,7 +59,10 @@ int detectCollisionBullet(int posX, int posY) {
 			if (barcModel[y][x] != 0 && currentGameBoard[arrY + y][arrX + x] == 1) {
 				return 0;
 			}
-			else if (barcModel[y][x] != 0 && currentGameBoard[arrY + y][arrX + x] != 0) {				return 9;
+			else if (barcModel[y][x] != 0 && currentGameBoard[arrY + y][arrX + x] != 0) {
+				return 9;
 			}
 		}
-	}	return 1;}
+	}
+	return 1;
+}
