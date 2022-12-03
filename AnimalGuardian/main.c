f4168e12add3a431d090d074213bdd314578f850
LongHonor <kyh011039@gmail.com>
2022-12-04 03:05:08 +0900

    runGame() 함수 호출해서 실행하는 main함수

--- AnimalGuardian/main.c
+++ AnimalGuardian/main.c
@@ -1,33 +1,6 @@
-﻿#include <stdio.h>
-#include <windows.h>
-#include "globalVariable.h"
-#include "gameBoardHandler.h"
-#include "gameItem.h"
-#include "pcHandler.h"
-#include "npcModule.h"
-#include "stage3Animation.h"
+﻿#include "runGame.h"
 
 int main() {
-    removeCursor();
-    srand((unsigned)time(NULL));
-    setCurrentGameBoard(1);
-    
-	setAllEnemyCount(5);
-    drawGameBoard();
-    showPC(player);
-
-    setAllAnimalCount(5);
-    makeAnimal();
-
-    resetEnemySpawnCount();
-    
-    makeEnemyList(allEnemyCount);
-    while (1) {
-        drawInitialUI();
-        enemyMoveSetting();
-        animalMoveSetting();
-        pcKeyInput();
-    }
-
+    runGame();
     return 0;
 }
