70b2b58fdab1eb1696c0951588dec6c77ed2bba4
LongHonor <kyh011039@gmail.com>
2022-12-07 02:57:19 +0900

    게임루프 초기 구성

--- AnimalGuardian/runGame.h
+++ AnimalGuardian/runGame.h
@@ -8,16 +8,17 @@
 #include "pcHandler.h"
 #include "npcModule.h"
 #include "stage3Animation.h"
+#include "gameLoby.h"
 
 void initSetting() {
     removeCursor();                     //커서 표시 지우기
     srand((unsigned)time(NULL));        //프로그램 전체를 위한 랜덤시드값 배정
 }
 
-boolean stage1() {
-    //스테이지1 설정
+boolean stage(int i) {
+    //스테이지 설정
     //맵 설정
-    setCurrentGameBoard(0);
+    setCurrentGameBoard(i);
 
     //PC출력
     showPC(player);
@@ -27,7 +28,7 @@ boolean stage1() {
     makeAnimal();
 
     //enemyNPC 정보 갱신
-    setAllEnemyCount(15);
+    setAllEnemyCount(15+5*i);
     resetEnemySpawnCount();
     makeEnemyList(allEnemyCount);
 
@@ -47,44 +48,26 @@ boolean stage1() {
         enemyMoveSetting();
     }
 }
-void stage2() {
-    //맵 설정
-    setCurrentGameBoard(1);
-
-    //PC출력
-    showPC(player);
-
-    //animalNPC 정보 갱신
-    setAllAnimalCount(3);
-    makeAnimal();
-
-    //enemyNPC 정보 갱신
-    setAllEnemyCount(20);
-    resetEnemySpawnCount();
-    makeEnemyList(allEnemyCount);
-
-    //게임보드 및 UI 출력
-    drawGameBoard();
-    drawInitialUI();
-
-    //게임동작부
-    while (1) {
-        if (currentAnimalCount == 0) {
-            //gameover
-            return FALSE;
-        }
-        if (currentEnemyCount == 0) return TRUE;
-        pcKeyInput();
-        animalMoveSetting();
-        enemyMoveSetting();
-    }
-}
 
 void runGame() {
+    boolean gameStatus = TRUE;
+    int gInt;
+    int i=0;
     initSetting();
 
-    stage1();
-    system("cls");
-    Sleep(30);
-    stage2();
+    gInt = drawGameLoby();
+
+    if(gInt != 2){
+        while(1){
+            gameStatus = stage(i);
+            system("cls");
+            if(gameStatus) i++;
+            else{
+                //gameOver
+                //
+                break;
+            }
+        }
+    }
+    else return;
 }
