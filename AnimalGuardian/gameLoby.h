4973cde896ad39ed961ef225e5dd5e0b3040ab49
DESKTOP-NNRHRSC\82104 <jackson0511@naver.com>
2022-12-07 02:39:13 +0900

    게임로비 이펙트 구현

--- AnimalGuardian/gameLoby.h
+++ AnimalGuardian/gameLoby.h
@@ -1,20 +1,42 @@
 ﻿#pragma once
-int stageNum;
+int flag;
+int checkGameTitle[15];
+int returnkeyShowLobyTitle;
 
-void showFirstDirectionLoby();
-void deleteFirstDirectionLoby();
-void showSecondDirectionLoby();
-void deleteSecondDirectionLoby();
-void showFirstDirectionGameOver();
-void deleteFirstDirectionGameOver();
-void showSecondDirectionGameOver();
-void deleteSecondDirectionGameOver();
+//로비 화면 화살표
+void showStartDirectionInLoby();
+void deleteStartDirectionInLoby();
+void showInsrtuctionDirectionInLoby();
+void deleteInstructionDirectionInLoby();
+void showExitDirectionInLoby();
+void deleteExitDirectionInLoby();
+//게임 오버 화면 화살표
+void showRestartDirectionGameOver();
+void deleteRestartDirectionGameOver();
+void showExitDirectionGameOver();
+void deleteExitDirectionGameOver();
+//로비 화면 생성
 int drawGameLoby();
+//게임설명서 화면 생성
 int drawInstruction();
+//게임 오버 화면 생성
 void drawGameOver();
+//화면 지우기
 void deleteBoard();
-void drawStageLoby();
+//스테이지 화면 생성
+void drawStageLoby(int stagenum);
+//테두리 그리기
 void drawSide();
-void showTitle();
+//한 글자씩 그리기
+void showTitleChracter(int startX, int startY, int sizeWidth, int sizeHeight);
+//화면 전체 타이틀 그리기
+void showTitle(char g[23][42]);
+//글자 색상 바꾸기
+int changeColorText(int i, int check[], int startX, int startY, int sizeWidth, int sizeHeight);
+//로비 화면 타이틀 생성 -> 이펙트 구현
+int showLobyTitle();
+
 void drawSquare();
 void drawRectangle();
+//타이틀 색상 여부 확인
+void initCheckGameTitle();
