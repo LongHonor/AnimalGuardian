f797ecb13a99fa945ae32479155f83f5dce3bd26
binbean <rlagyqls2682@naver.com>
2022-12-07 17:46:01 +0900

    boss detect 추가

--- AnimalGuardian/gameBoardHandler.c
+++ AnimalGuardian/gameBoardHandler.c
@@ -264,7 +264,7 @@ void drawBoss() {
 			setCurrentCursorPos(boss.pos.X + posX * 2, boss.pos.Y+ posY);
 			if (bossEnemyModel[posY][posX] == 2) printf("※");
 			else printf("■");
-			
+			currentGameBoard[arrY + posY][arrX + posX] = 8;
 		}
 	}
 
@@ -280,7 +280,7 @@ void deleteBoss() {
 		for (posY = 0; posY < 4; posY++) {
 			setCurrentCursorPos(boss.pos.X + posX * 2, boss.pos.Y+posY);
 			printf("  ");
-
+			currentGameBoard[arrY + posY][arrX + posX] = 0;
 		}
 	}
 
